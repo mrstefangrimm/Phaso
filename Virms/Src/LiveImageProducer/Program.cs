b8a23030063eba69dca8e1647e85b515585fd2a7
mrstefangrimm <motionsystems@grimm.com>
2021-11-07 17:18:28 +0100

    Squashed commit of the following:
    commit aa7e61f2a05aaa1fcc13976373f9ab1abd7a758a
    Author: mrstefangrimm <motionsystems@grimm.com>
    Date:   Sun Nov 7 17:06:19 2021 +0100
    
        Add app.config
    
    commit c74d896381eb6ac0f86cdd5d8e7b5b715d9ff115
    Author: mrstefangrimm <motionsystems@grimm.com>
    Date:   Tue Nov 2 18:38:26 2021 +0100
    
        Initial commit for live image producer.

---
+++ Virms/Src/LiveImageProducer/Program.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) 2021 Stefan Grimm. All rights reserved.
+// Licensed under the GPL. See LICENSE file in the project root for full license information.
+//
+using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Configuration;
+using System.Drawing;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LiveImageProducer {
+  class Program {
+    static void Main(string[] args) {
+
+      int cameraIndex;
+      if (!int.TryParse(ConfigurationManager.AppSettings["CameraIndex"], out cameraIndex)) {
+        Console.WriteLine($"Unable to parse camera index from '{ConfigurationManager.AppSettings["CameraIndex"]}'");
+        return;
+      }
+
+      var cts = new CancellationTokenSource();
+
+      var task = Task.Factory.StartNew(() => {
+
+        var cameraCapture = new VideoCapture(cameraIndex);
+
+        while (!cts.Token.IsCancellationRequested) {
+          try {
+            using Mat frame = cameraCapture.QueryFrame();
+
+            using var fullImg = frame.ToImage<Bgr, byte>();
+            string pathFullImg = Path.Combine(ConfigurationManager.AppSettings["OutputDir"], "full-live.jpg");
+            fullImg.Save(pathFullImg);
+
+            fullImg.ROI = new Rectangle(0, 0, 200, 480);
+            using var img1 = fullImg.Copy();
+            string pathFirstImg = Path.Combine(ConfigurationManager.AppSettings["OutputDir"], "first-live.jpg");
+            img1.Save(pathFirstImg);
+
+            fullImg.ROI = new Rectangle(200, 0, 200, 480);
+            using var img2 = fullImg.Copy();
+            string pathSecondImg = Path.Combine(ConfigurationManager.AppSettings["OutputDir"], "second-live.jpg");
+            img2.Save(pathSecondImg);
+
+            fullImg.ROI = new Rectangle(400, 0, 240, 480);
+            using var img3 = fullImg.Copy();
+            string pathThirdImg = Path.Combine(ConfigurationManager.AppSettings["OutputDir"], "third-live.jpg");
+            img3.Save(pathThirdImg);
+
+            Thread.Sleep(250);
+          }
+          catch (Exception e) {
+            Console.WriteLine(e.Message);
+            Thread.Sleep(2000);
+          }
+        }
+        cameraCapture.Dispose();
+      });
+
+      Console.WriteLine("Running until you stop.");
+      Console.ReadKey();
+
+      // Request cancellation.
+      cts.Cancel();
+      Console.WriteLine("Cancellation set in token source...");
+      Thread.Sleep(1000);
+      // Cancellation should have happened, so call Dispose.
+      cts.Dispose();
+    }
+  }
+}
